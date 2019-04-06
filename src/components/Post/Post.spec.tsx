3122f287787f6a289476b1b7d32b3c4f19c4f249
Morgan McCauley <morgan@mccauley.co.nz>
2019-04-06 13:52:23 +1300

    Test Post

---
+++ src/components/Post/Post.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Post from './Post';
+
+const post = {
+  title: 'title',
+  id: 'id',
+  author: 'author',
+  totalComments: 0,
+  score: 0,
+};
+
+describe('Post', () => {
+  it('renders image if present', () => {
+    const postWithImage = {
+      ...post,
+      image: {
+        url: 'url',
+        width: 0,
+        height: 0,
+      },
+    };
+    const wrapper = shallow(<Post post={post} />);
+    expect(wrapper.exists('Image')).to.be.false;
+    wrapper.setProps({ post: postWithImage });
+    expect(wrapper.exists('Image')).to.be.true;
+  });
+});
